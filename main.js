d2f0c0a175c0e9973794da0b2814db2d2d2f05f9
Callan <56173580+callan101@users.noreply.github.com>
2023-09-19 16:31:56 -0700

    Mobileifying done

--- main.js
+++ main.js
@@ -1,46 +1,46 @@
 function updateUnixTime() {
-    const unixTimeElement = document.getElementById("unix-time");
+    const unixTimeElements = document.querySelectorAll(".unix-time");
     const now = new Date();
     const unixTime = Math.floor(now.getTime() / 1000); // Convert to seconds
-    unixTimeElement.textContent = unixTime;
+
+    // Update all elements with the class "unix-time"
+    unixTimeElements.forEach(element => {
+        element.textContent = unixTime;
+    });
 }
 
-
-
 function updateClock() {
-    const clockElement = document.getElementById("clock");
+    const clockElements = document.querySelectorAll(".clock");
     const now = new Date();
 
-    // Get the hours, minutes, and seconds
     const hours = now.getHours().toString().padStart(2, "0");
     const minutes = now.getMinutes().toString().padStart(2, "0");
     const seconds = now.getSeconds().toString().padStart(2, "0");
-
-    // Format the time in 24-hour clock format
     const formattedTime = `${hours}:${minutes}:${seconds}`;
 
-    clockElement.textContent = formattedTime;
+    // Update all elements with the class "clock"
+    clockElements.forEach(element => {
+        element.textContent = formattedTime;
+    });
 }
 
 function formatYearMonthDay() {
-    const formattedDateElement = document.getElementById("formatted-date");
+    const formattedDateElements = document.querySelectorAll(".formatted-date");
     const now = new Date();
 
-    const year = now.getFullYear(); // Get the year (e.g., 2023)
-    const month = now.getMonth() + 1; // Get the month (0-11, so we add 1)
-    const day = now.getDate(); // Get the day of the month (1-31)
-
-    // Format the date as "year.month.day"
+    const year = now.getFullYear();
+    const month = now.getMonth() + 1;
+    const day = now.getDate();
     const formattedDate = `${year}.${month}.${day}`;
 
-    formattedDateElement.textContent = formattedDate;
+    // Update all elements with the class "formatted-date"
+    formattedDateElements.forEach(element => {
+        element.textContent = formattedDate;
+    });
 }
-
-
 
 formatYearMonthDay();
 setInterval(formatYearMonthDay, 1000);
-
 
 updateClock();
 setInterval(updateClock, 1000);
